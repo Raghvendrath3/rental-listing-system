1d1f664a3c46b00ad051b3e0035405e908a1b201
Raghvendrath3 <raghvendrath3@gmail.com>
2026-01-30 16:39:06 +0530

    CRUD functions added

--- src/repositories/listing.repository.js
+++ src/repositories/listing.repository.js
@@ -16,7 +16,8 @@ const listingRepository = [{
     price: 2500,
     isAvailable: false,
     ownerId: 102
-  },{
+  },
+  {
     id:3,
     title: "Modern Room Near Park",
     type: "room",
@@ -35,4 +36,31 @@ function findAll(){
 function findById(id){
   return listingRepository.find(listing => listing.id === id);
 };
-module.exports = { findAll, findById };
+
+function addListing(newListing){
+  const newId = listingRepository.length +1;
+  const listingToAdd = {id: newId, ...newListing};
+  listingRepository.push(listingToAdd);
+  return listingToAdd;
+}
+
+function updateListing(id, updatedFields){
+  const listing = findById(id);
+  if (!listing) {
+    return null;
+  }
+  const updatedListing = {...listing, ...updatedFields};
+  const index = listingRepository.findIndex(listing => listing.id === id);
+  listingRepository[index] = updatedListing;
+  return updatedListing;
+}
+
+function deleteListing(id){
+  const index = listingRepository.findIndex(listing => listing.id === id);
+  if (index === -1) {
+    return null;
+  }
+  return listingRepository.splice(index, 1)[0];
+}
+
+module.exports = { findAll, findById, addListing, updateListing, deleteListing };
